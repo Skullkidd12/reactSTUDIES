7442145b96ae5545043d82a34b5ee928e24bcabe
Skullkidd12 <araujo.skd@gmail.com>
2022-07-07 23:05:42 -0300

    aprendendo mais

--- react_enviroment1/src/App.js
+++ react_enviroment1/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+let Dados = true;
+const EU = () => {
+  if (Dados == true) {
+    return (
+      <>
+        <h1> Nome: Renan</h1>
+        <h1> Sobrenome: Araujo</h1>
+        <h2> Idade: 24</h2>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Usuario nao cadastrado</h1>
+      </>
+    );
+  }
+};
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> dados do usuário: </p>
+      <EU />
+      <EU />
+      <EU />
+      <EU />
     </div>
   );
-}
+};
 
 export default App;
